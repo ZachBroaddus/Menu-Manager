c14b38a1d247c210069d62fa48faa0577e3119b4
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2018-01-10 21:54:26 -0600

    create users and sessions controllers and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
+
+  resources :users, only: [:new, :create]
+
+  resources :sessions, only: [:create, :destroy]
+
+  root "items#index", as: "menu"
+
+  get 'logout', to: "sessions#destroy", as: 'logout'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
