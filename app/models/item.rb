9bd9b26e4ca6856c67e136ca994a3436e849a3a2
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2018-02-01 22:28:52 -0600

    add validation to item model, description must be present

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  validates :name, :category, :image_url, :price, presence: true
+  validates :name, :category, :image_url, :price, :description, presence: true
   validates_uniqueness_of :name
 
   has_many :order_items
