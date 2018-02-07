9541a31ca969b89e82b1b5b49e2b80ce478b0330
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2018-02-07 02:33:03 -0600

    clean up comments in environment files

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ApplicationRecord
-  validates :name, :category, :image_url, :price, :description, presence: true
+  validates :name, :category, :price, :description, presence: true
   validates_uniqueness_of :name
   has_attached_file :image, styles: { large: "400x400>", medium: "200x200>", thumb: "100x100>" }, default_url: "https://s3-us-west-1.amazonaws.com/menu-manager-pro/items/images/default-cat.jpg"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
