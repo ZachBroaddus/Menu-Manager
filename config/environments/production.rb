241027953005d04fa4b011c50dbe0769bb6b0383
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2018-02-08 17:37:30 -0600

    set precompile assets to true in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,8 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # changed from false to true for font awesome
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
