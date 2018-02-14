6dce11a218fd72cca53408f0a37bcf8714b9cd9b
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2018-02-14 00:58:20 -0600

    precompile assets for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
     :port                 => 587,
     :user_name            => ENV['gmail_username'],
     :password             => ENV['gmail_password'],
-    :authentication       => "plain",
+    :authentication       => :plain,
     :enable_starttls_auto => true
   }
 
