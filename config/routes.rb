32a63fa744ed6f07c86722f733b22beea408b414
Zach Broaddus <zachbroaddus@zachs-mbp.attlocal.net>
2018-02-15 02:17:57 -0600

    precompile assets for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,14 @@ Rails.application.routes.draw do
 
   resources :order_items, only: [:create, :update, :destroy]
 
-  resources :password_resets
+  resources :password_resets, except: [:show, :index, :destroy]
 
   root 'items#index'
 
   get 'confirm', to: 'orders#confirm', as: 'confirm'
-  get 'logout', to: 'sessions#destroy'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'about', to: 'pages#about', as: 'about'
+  get 'contact', to: 'pages#contact', as: 'contact'
 
   get "*any", via: :all, to: "errors#not_found", as: 'not_found'
 end
